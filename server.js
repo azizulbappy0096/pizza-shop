23a0a5942306efcaf944a320483699237d8fe728
Azizul Bappy <azizulbappy6@gmail.com>
2021-02-24 21:57:08 +0600

    add laravel mix webpack

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 // import modules //
-const express = require("express")
-require("dotenv").config()
+const express = require("express");
+require("dotenv").config();
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
 
 // init
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// middlewares
+app.use(express.static("public"))
+
+// routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// layout setting
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/pages"));
+app.set("view engine", "ejs");
+
+// listen
 app.listen(PORT, () => {
-    console.log("Listening on " + PORT)
-})
-
+  console.log("Listening on " + PORT);
+});
