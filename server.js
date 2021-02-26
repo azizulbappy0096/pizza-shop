5fd2eb9045bb0effcf8793e091d4fb43f46faf66
Azizul Bappy <azizulbappy6@gmail.com>
2021-02-26 22:46:06 +0600

    create empty cart page

--- server.js
+++ server.js
@@ -12,15 +12,21 @@ const PORT = process.env.PORT || 3000;
 // middlewares
 app.use(express.static("public"))
 
+// layout setting
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/pages"));
+app.set("view engine", "ejs");
+
 // routes
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// layout setting
-app.use(expressLayout);
-app.set("views", path.join(__dirname, "/resources/pages"));
-app.set("view engine", "ejs");
+app.get("/cart", (req, res) => {
+  res.render("customer/cart");
+});
+
+
 
 // listen
 app.listen(PORT, () => {
